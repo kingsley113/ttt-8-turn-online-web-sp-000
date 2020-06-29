c5b4c4d7b1a3ea77e193a755c332a83686185de9
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-06-29 20:09:15 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+
+def turn
+# ask for input
+# get input
+# convert input to index
+
+# if index is valid
+  # make the move for index
+  # show the board
+# else
+  # ask for input again until you get a valid input
+# end
+
+
+
+def display(board)
+
+end
+
+def move(board, index)
+
+def valid_move?(board, index)
+  def position_taken(board, index)
+
+end
