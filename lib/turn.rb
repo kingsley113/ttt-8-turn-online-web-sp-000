37a1109ebe53bbe85840e43fa3f294daed345d6e
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-06-29 21:12:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ end
 
 # convert the input of the user (spots 1-9) to the index values for the board
 def move(array, index, value = "X")
-  array[index] = value
+  array[index.to_i] = value
 end
 
 # determine if the entered move is a valid move
