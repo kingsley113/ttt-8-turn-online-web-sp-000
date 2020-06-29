5d301d4fc34085eefe17e03e5067a88411863a24
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-06-29 20:11:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,14 +12,27 @@ def turn
 # end
 
 
-
+#generate and display the current board
 def display(board)
 
 end
 
+# convert the input of the user (spots 1-9) to the index values for the board
 def move(board, index)
 
+# determine if the entered move is a valid move
 def valid_move?(board, index)
-  def position_taken(board, index)
+  position_taken(board, index)
 
+end
+
+#determine if the current position on the board is already taken
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "  "
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
 end
